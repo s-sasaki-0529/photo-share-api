53e0719265e17796541f68b2834fe068d72888fd
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-10-03 20:37:42 +0900

    最小限の GraphQL サーバーを実装

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`GraphQL Service running on ${url}`);
+});
