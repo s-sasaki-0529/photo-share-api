110426aa7bd47fbe3046ac398873d5646205d947
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-10-03 22:21:14 +0900

    5.2.4 エッジと接続

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { ApolloServer, Config } from "apollo-server";
 import { ExpressContext } from "apollo-server-express";
+import { createPhotos, createUsers } from "./src/seeds";
 
 let _id = 0;
-const photos: Photo[] = [];
+const users = createUsers();
+const photos: any[] = createPhotos();
 
 const typeDefs: Config<ExpressContext>["typeDefs"] = `
   enum PhotoCategory {
@@ -19,17 +21,26 @@ const typeDefs: Config<ExpressContext>["typeDefs"] = `
     category: PhotoCategory=PORTRAIT
   }
 
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
     category: PhotoCategory
+    postedBy: User!
   }
 
   type Query {
     totalPhotos: Int!
     allPhotos: [Photo!]!
+    allUsers: [User!]!
   }
 
   type Mutation {
@@ -41,10 +52,11 @@ const resolvers: Config<ExpressContext>["resolvers"] = {
   Query: {
     totalPhotos: () => photos.length,
     allPhotos: () => photos,
+    allUsers: () => users,
   },
   Mutation: {
-    postPhoto: (parent, args: { input: PostPhotoInput }) => {
-      const newPhoto: Photo = {
+    postPhoto: (parent, args) => {
+      const newPhoto = {
         id: _id++,
         ...args.input,
       };
@@ -54,6 +66,14 @@ const resolvers: Config<ExpressContext>["resolvers"] = {
   },
   Photo: {
     url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((u) => u.githubLogin === parent.githubUser);
+    },
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser === parent.githubLogin);
+    },
   },
 };
 
---
+++ src/seeds.ts
@@ -0,0 +1,37 @@
+export const createUsers = () => [
+  {
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup",
+  },
+  {
+    githubLogin: "gPlake",
+    name: "Glen Plake",
+  },
+  {
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt",
+  },
+];
+
+export const createPhotos = () => [
+  {
+    id: 1,
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: 2,
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: 3,
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
